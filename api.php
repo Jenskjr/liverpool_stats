4430d9132d1b07b6ca3a17bd361a0a43f9c48b1d
Jenskjr <Jenskjr@gmail.com>
2018-10-09 11:10:03 +0200

    Initial commit

---
+++ api.php
@@ -0,0 +1,51 @@
+<?php
+
+require 'connection.php';
+require 'sql.php';
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+
+	$products_arr=array();
+    $results_arr["results"]=array();
+
+	while($row = $result->fetch_assoc()) {
+    	
+    	extract($row);
+
+    	$product_item=array(
+            "id" => $id,
+            "year" => $year, 
+            "position" => $position, 
+            "games" => $games,
+            "points" => $points, 
+            "won" => $won, 
+            "draws" => $draws, 
+            "lost" => $lost, 
+            "goal_diff" => $goal_diff
+        );
+
+        array_push($results_arr["results"], $product_item);
+    }
+
+    echo json_encode($results_arr);
+
+
+} else {
+    echo "0 results";
+}
+
+$conn->close();
+?>
+
+
+
---
+++ connection.php
@@ -0,0 +1,6 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "root";
+$dbname = "liverpool_stats";
+?>
